468541ba0a3aa1e9665754071ed23cfcdbeca8c2
Truong Minh Thang <truongminhthang10@gmail.com>
2025-12-03 07:58:32 +0700

    1

---
+++ backend/process.php
@@ -0,0 +1,11 @@
+<?php
+// Lấy dữ liệu từ form
+$name = $_POST['your_name'] ?? 'Khách';
+
+// Xử lý (ví dụ: viết hoa)
+$name = strtoupper($name);
+
+// Trả kết quả về trình duyệt
+echo "<h2>Xin chào: $name</h2>";
+echo "<a href='index.html'>Quay lại</a>";
+?>
